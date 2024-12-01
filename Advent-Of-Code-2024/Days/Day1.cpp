f81775941c7a9e0679d14c01b7844fd1f94834f3
Christian Fikus <christian.fikus@wp.pl>
2024-12-01 10:34:18 +0100

    Day 1

--- Advent-Of-Code-2024/Days/Day1.cpp
+++ Advent-Of-Code-2024/Days/Day1.cpp
@@ -1,13 +1,52 @@
 #include "../general.h"
+#include <map>
 
-string Day1_Part1(stringstream& input)
+int Day1_Part1(stringstream& input)
 {
-	return "Not implemented yet";
+	vector<int> list1, list2;
+
+	while (!input.eof())
+	{
+		int num1=0, num2=0;
+		input >> num1 >> num2;
+
+		list1.push_back(num1);
+		list2.push_back(num2);
+	}
+
+	ranges::sort(list1);
+	ranges::sort(list2);
+
+	int sum = 0;
+	for (int i = 0; i < list1.size(); i++)
+	{
+		sum += abs(list1[i] - list2[i]);
+	}
+
+	return sum;
 }
 
-string Day1_Part2(stringstream& input)
+int Day1_Part2(stringstream& input)
 {
-	return "Not implemented yet";
+	vector<int> list;
+	map<int, int> repeats;
+
+	while (!input.eof())
+	{
+		int num1 = 0, num2 = 0;
+		input >> num1 >> num2;
+
+		list.push_back(num1);
+		repeats[num2]++;
+	}
+
+	int score = 0;
+	for (int i = 0; i < list.size(); i++)
+	{
+		score += list[i] * repeats[list[i]];
+	}
+
+	return score;
 }
 
 
