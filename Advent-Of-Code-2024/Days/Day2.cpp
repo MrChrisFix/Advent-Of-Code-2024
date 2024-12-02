20cd2b55b3704fc73c761dc99c107746fcdc8678
Christian <christian.fikus@wp.pl>
2024-12-02 16:32:42 +0100

    Day 2

--- Advent-Of-Code-2024/Days/Day2.cpp
+++ Advent-Of-Code-2024/Days/Day2.cpp
@@ -1,6 +1,30 @@
 #include "../general.h"
 #include <set>
 
+
+int sign(int x)
+{
+	if (x < 0)
+		return -1;
+	else if (x > 0)
+		return 1;
+	return x;
+}
+
+bool isSafe(vector<int> nums)
+{
+	for (int i = 1; i < nums.size(); i++)
+	{
+		if (abs(nums[i] - nums[i - 1]) > 3 ||
+			nums[i] == nums[i - 1] ||
+			(i != nums.size() - 1 && sign(nums[i - 1] - nums[i]) != sign(nums[i] - nums[i + 1]))
+			)
+			return false;
+	}
+
+	return true;
+}
+
 int Day2_Part1(stringstream& input)
 {
 	int safe = 0;
@@ -10,49 +34,20 @@ int Day2_Part1(stringstream& input)
 		getline(input, sline);
 		stringstream line(sline);
 
-		int num1, num2;
-		line >> num1 >> num2;
-
-		if (abs(num1 - num2) > 3 || num1 == num2)
-			continue;
-
-		bool decr = num1 > num2;
-		num1 = num2;
-
-		bool good = true;
+		vector<int> nums;
 		while (!line.eof())
 		{
-			line >> num2;
-
-			if ((decr && num1 < num2) || (!decr && num1 > num2))
-			{
-				good = false;
-				break;
-			}
-
-			if (abs(num1 - num2) > 3 || num1 == num2)
-			{
-				good = false;
-				break;
-			}
-
-			num1 = num2;
+			int num;
+			line >> num;
+			nums.push_back(num);
 		}
-		if (good)
+
+		if (isSafe(nums))
 			safe++;
 	}
 
 
 	return safe;
-}
-
-int sign(int x)
-{
-	if (x < 0)
-		return -1;
-	else if (x > 0)
-		return 1;
-	return x;
 }
 
 int Day2_Part2(stringstream& input)
@@ -72,184 +67,25 @@ int Day2_Part2(stringstream& input)
 			nums.push_back(num);
 		}
 
-		bool onefail = false;
-		bool good = true;
-		vector<int> sortedNums = nums;
-		ranges::sort(sortedNums);
-
-		if (ranges::max(nums) == nums[0] || ranges::max(nums) == nums[1])
-			ranges::reverse(nums);
-
-
-		//1. Check for duplicates
-		//2. Check fow many numbers were changed
-		//3. Check diff in sotred between nums
-		//4. Check if the difference between the changed numbers is less than 3
-
-		set<int> duplicates;
-
-		//Duplicates
-		for (int i = 1; i < sortedNums.size() && good; i++)
+		if (isSafe(nums))
 		{
-			if (sortedNums[i] == sortedNums[i - 1])
-			{
-				duplicates.insert(sortedNums[i]);
-				if (onefail)
-					good = false;
-				onefail = true;
-			}
-		}
-
-		//0 < diff < 3
-		for (int i = 1; i < sortedNums.size() && good; i++)
-		{
-			if ((sortedNums[i] - sortedNums[i - 1]) > 3)
-			{
-				if ((i == sortedNums.size() - 1 && onefail == false) || i == 0)
-				{
-					onefail = true;
-				}
-				else
-				{
-					good = false;
-					onefail = true;
-				}
-			}
-		}
-
-		//Difference
-		for (int i = 0; i < sortedNums.size() && good; i++)
-		{
-			if (abs(sortedNums[i] - nums[i]) > 3)
-			{
-				if (sortedNums[i] == sortedNums[i - 1])
-				{
-					if (onefail)
-						good = false;
-					onefail = true;
-				}
-			}
-		}
-
-		for (int i = 1; i < nums.size() - 1 && good; i++)
-		{
-			if (sign(nums[i - 1] - nums[i]) != sign(nums[i] - nums[i + 1]) &&
-				(!duplicates.contains(nums[i - 1]) || !duplicates.contains(nums[i]) || !duplicates.contains(nums[i + 1]))
-				)
-			{
-				if (onefail)
-				{
-					good = false;
-				}
-				onefail = true;
-			}
-		}
-
-		if (good) //25 23 25 28 29 32 35 -> currently classified as bad
 			safe++;
-
-
-		//for (int i = 1; i < nums.size() - 1; i++)
-		//{
-		//	if (
-		//		sign(nums[i - 1] - nums[i]) != sign(nums[i] - nums[i + 1]) ||
-		//		nums[i - 1] == nums[i] || nums[i] == nums[i + 1] ||
-		//		abs(nums[i - 1] - nums[i]) > 3 || abs(nums[i] - nums[i + 1]) > 3
-		//		)
-		//	{
-		//		if (onefail)
-		//		{
-		//			good = false;
-		//			break;
-		//		}
-		//		onefail = true;
-		//		if (i == 1 && abs(nums[i] - nums[i + 1]) <= 3)
-		//			nums.erase(nums.begin());
-		//		else if (abs(nums[i - 1] - nums[i + 1]) <= 3 && nums[i - 1] != nums[i + 1])
-		//			nums.erase(nums.begin() + i);
-		//		else if (i == nums.size() - 2 && abs(nums[i] - nums[i - 1]) <= 3)
-		//			nums.erase(nums.begin() + i + 1);
-		//		else
-		//		{
-		//			good = false;
-		//			break;
-		//		}
-		//		i--;
-		//	}
-		//}
-		//if (good)
-		//	safe++;
-
-		/*int num1, num2, num3;
-		line >> num1 >> num2 >> num3;
-		bool oneFail = false;
-
-		if (abs(num1 - num2) > 3 || num1 == num2)
-		{
-			oneFail = true;
-
-			if (abs(num1 - num3) > 3)
-			{
-				num1 = num2;
-				num2 = num3;
-			}
-			else
-			{
-				num2 = num3;
-			}
-
-			if (num1 == num2 || abs(num1 - num2) > 3)
-				continue;
+			continue;
 		}
 
-		bool decr = num1 > num2;
-
-		bool good = true;
-		while (!line.eof())
+		for (int i = 0; i < nums.size(); i++)
 		{
-
-			bool end = line.eof();
-
-
-			if (
-				(decr && num2 < num3) ||
-				(!decr && num2 > num3) ||
-				num2 == num3 ||
-				abs(num2 - num3) > 3
-				)
+			vector<int> numsCopy = nums;
+			numsCopy.erase(numsCopy.begin() + i);
+			if (isSafe(numsCopy))
 			{
-				if (oneFail == false)
-				{
-					if (abs(num1 - num3) > 3 && !end)
-					{
-						good = false;
-						break;
-					}
-
-
-					oneFail = true;
-					continue;
-				}
-				else
-				{
-					good = false;
-					break;
-				}
-
+				safe++;
+				break;
 			}
-
-			num1 = num2;
-			num2 = num3;
-
-			line >> num3;
 		}
-		if (good)
-			safe++;*/
 	}
 
-	//breaks at eg. 1 4 2 4 4
-
-	return safe; //406 too low
+	return safe;
 }
 
 
