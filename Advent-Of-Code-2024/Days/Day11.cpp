b2ea3ef0c5a57afc1573908fafad598b656f509c
Christian <christian.fikus@wp.pl>
2024-12-11 17:29:10 +0100

    Day 11

--- Advent-Of-Code-2024/Days/Day11.cpp
+++ Advent-Of-Code-2024/Days/Day11.cpp
@@ -1,21 +1,64 @@
 #include "../general.h"
+#include <map>
 
-string Day11_Part1(stringstream& input)
+int numLen(size_t num)
 {
-	return "Not implemented yet";
+	return floor(log10(num)) + 1;
 }
 
-string Day11_Part2(stringstream& input)
+pair<size_t, size_t> split(size_t num)
 {
-	return "Not implemented yet";
+	int size = numLen(num);
+	size_t power = pow(10, size / 2);
+
+	return { num / power, num % power };
 }
 
+size_t blink(map<size_t, size_t> occurences, int blinks)
+{
+	for (int i = 0; i < blinks; i++)
+	{
+		map<size_t, size_t> newOcc;
+		for (auto& occ : occurences)
+		{
+			if (occ.first == 0)
+				newOcc[1] += occ.second;
+			else if (numLen(occ.first) % 2 == 0)
+			{
+				auto splitted = split(occ.first);
+				newOcc[splitted.first] += occ.second;
+				newOcc[splitted.second] += occ.second;
+			}
+			else
+			{
+				newOcc[occ.first * 2024] += occ.second;
+			}
+		}
+		occurences = newOcc;
+	}
+
+	size_t res = 0;
+
+	for (auto& num : occurences)
+		res += num.second;
+
+	return res;
+}
 
 void Day11(stringstream& input)
 {
-	cout<<"Part 1 result is: " << Day11_Part1(input)<<endl;
-	input.seekg(0);
-	cout<<"Part 2 result is: " << Day11_Part2(input)<<endl<<endl;
+	map<size_t, size_t> occurences;
+	while (!input.eof())
+	{
+		int num;
+		input >> num;
+		occurences[num]++;
+	}
+	size_t part1 = blink(occurences, 25);
+	size_t part2 = blink(occurences, 75);
+
+	cout << "Part 1 result is: " << part1 << endl;
+	cout << "Part 2 result is: " <<part2 << endl << endl;
 
 	return;
 }
