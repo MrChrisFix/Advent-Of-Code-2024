c05d3744d095d4f0651865ae2ac470599ca104b2
Christian Fikus <christian.fikus@wp.pl>
2024-12-12 14:01:25 +0100

    Day 12

--- Advent-Of-Code-2024/Days/Day12.cpp
+++ Advent-Of-Code-2024/Days/Day12.cpp
@@ -1,21 +1,120 @@
 #include "../general.h"
+#include <queue>
+#include <set>
 
-string Day12_Part1(stringstream& input)
+struct Farm
 {
-	return "Not implemented yet";
-}
+	int field, fences, corners;
+};
 
-string Day12_Part2(stringstream& input)
+Farm getFieldFencesAndCorners(const vector<string> farm, queue<pair<int, int>>& nextPos, set<pair<int, int>>& visited)
 {
-	return "Not implemented yet";
-}
+	int field = 0, fences = 0, corners = 0;
 
+	char searched = farm[nextPos.front().first][nextPos.front().second];
+
+	while (!nextPos.empty())
+	{
+		auto now = nextPos.front();
+		nextPos.pop();
+
+		if (!visited.insert(now).second)
+		{
+			continue;
+		}
+		field++;
+
+#define UP (now.first > 0 && farm[now.first - 1][now.second] == searched)
+#define LEFT (now.second > 0 && farm[now.first][now.second - 1] == searched)
+#define DOWN (now.first < farm.size() - 1 && farm[now.first + 1][now.second] == searched)
+#define RIGHT (now.second < farm[0].size() - 1 && farm[now.first][now.second + 1] == searched)
+
+		if (UP)
+		{
+			nextPos.push({ now.first - 1, now.second });
+		}
+		else
+			fences++;
+
+		if (LEFT)
+		{
+			nextPos.push({ now.first, now.second - 1 });
+		}
+		else
+			fences++;
+
+		if (DOWN)
+		{
+			nextPos.push({ now.first + 1, now.second });
+		}
+		else
+			fences++;
+
+		if (RIGHT)
+		{
+			nextPos.push({ now.first, now.second + 1 });
+		}
+		else
+			fences++;
+
+		//Convex
+		if (!(UP) && !(LEFT))
+			corners++;
+		if (!(UP) && !(RIGHT))
+			corners++;
+		if (!(DOWN) && !(LEFT))
+			corners++;
+		if (!(DOWN) && !(RIGHT))
+			corners++;
+
+		//Concave
+		if (UP && LEFT && farm[now.first - 1][now.second - 1] != searched)
+			corners++;
+		if (UP && RIGHT && farm[now.first - 1][now.second + 1] != searched)
+			corners++;
+		if (DOWN && LEFT && farm[now.first + 1][now.second - 1] != searched)
+			corners++;
+		if (DOWN && RIGHT && farm[now.first + 1][now.second + 1] != searched)
+			corners++;
+	}
+
+	return { field, fences, corners };
+}
 
 void Day12(stringstream& input)
 {
-	cout<<"Part 1 result is: " << Day12_Part1(input)<<endl;
-	input.seekg(0);
-	cout<<"Part 2 result is: " << Day12_Part2(input)<<endl<<endl;
+	vector<string> farm;
+	while (!input.eof())
+	{
+		string line;
+		input >> line;
+		farm.push_back(line);
+	}
+
+	queue<pair<int, int>> nextPos;
+	set<pair<int, int>> visited;
+
+	int scoreP1 = 0;
+	int scoreP2 = 0;
+
+	for (int y = 0; y < farm.size(); y++)
+	{
+		for (int x = 0; x < farm[y].size(); x++)
+		{
+			if (visited.contains({ y,x }))
+				continue;
+
+			nextPos.push({ y,x });
+
+			Farm res = getFieldFencesAndCorners(farm, nextPos, visited);
+
+			scoreP1 += res.field * res.fences;
+			scoreP2 += res.field * res.corners;
+		}
+	}
+
+	cout << "Part 1 result is: " << scoreP1 << endl;
+	cout << "Part 2 result is: " << scoreP2 << endl << endl;
 
 	return;
 }
